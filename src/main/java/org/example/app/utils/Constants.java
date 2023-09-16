5ca63c27d98a2bf6a9270dd02e7207a837df4fca
OleksandrChaika13 <axelrod1347@gmail.com>
2023-09-16 11:13:06 +0300

    Initial commit

---
+++ src/main/java/org/example/app/utils/Constants.java
@@ -0,0 +1,18 @@
+package org.example.app.utils;
+
+public class Constants {
+
+    private Constants(){
+    }
+
+    public static final int PORT = 8001;
+    public static final String HOST = "127.0.0.1";
+    public static final String SERVER_START_MSG = "Server started and waiting for users...";
+    public static final String USER_JOIN_MSG = "User joined - ";
+    public static final String MSG_RECEIVE_MSG = "Message received: ";
+    public static final String SERVER_CLOSE_MSG = "Closing connection for user - ";
+    public static final String SERVER_HELLO_MSG = "Hello ";
+    public static final String SERVER_HELLO2_MSG = ", welcome to our FreeForAll server!";
+    public static final String SERVER_MAIN_MSG = "SERVER: ";
+
+}
